2a7043cb2dbe0df8d587f50cfbc798b5e615aa4f
soknao <soknao2003@gmail.com>
2026-06-16 13:06:36 +0700

    fix: register PasswordResetServiceProvider in bootstrap/providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
+use Illuminate\Auth\Passwords\PasswordResetServiceProvider;
 
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
+    PasswordResetServiceProvider::class,
 ];
