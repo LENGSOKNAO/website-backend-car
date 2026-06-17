7af3d6c33e5d37503b0745bce57fbf0d538093d9
soknao <soknao2003@gmail.com>
2026-06-17 15:09:55 +0700

    cleanup: remove unused controllers, test files, and debug scripts

--- check_seller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-$app = require 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$users = App\Models\User::where('type', 'seller')->get(['email']);
-foreach ($users as $u) echo $u->email . PHP_EOL;
--- check_users2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-$app = require 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$users = App\Models\User::all(['email', 'type']);
-foreach ($users as $u) echo $u->email . ' - ' . $u->type . PHP_EOL;
